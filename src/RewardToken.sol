95e3594e565e49fd08f15f700ad53cfb35b94878
grc02 <georgi.chonkov27@gmail.com>
2023-10-07 20:49:24 +0300

    Simple ERC20 token, which stakers will receive as a reward

---
+++ src/RewardToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable2Step, Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+
+contract RewardToken is Ownable2Step, ERC20 {
+    constructor(string memory _name, string memory _symbol) Ownable(_msgSender()) ERC20(_name, _symbol) {}
+
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
+        emit Transfer(address(0), _to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external onlyOwner {
+        _burn(_from, _amount);
+        emit Transfer(_from, address(0), _amount);
+    }
+}
