4bb44f42576cacc76629fcaed93433ba5ec7069f
grc02 <georgi.chonkov27@gmail.com>
2023-10-09 17:21:58 +0300

    Created enumberable collection inhereting 'ERC721Enumerable' by OZ

---
+++ src/EnumerableCollection.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {ERC721Enumerable} from "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {IERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract EnumerableCollection is Ownable, ERC721Enumerable {
+    uint256 public constant MINT_PRICE = 1 ether;
+    uint256 public constant SUPPLY = 21;
+    uint256 public currentTokenId = 1;
+
+    constructor(string memory _name, string memory _symbol) Ownable(_msgSender()) ERC721(_name, _symbol) {}
+
+    /**
+     * @notice Mint NFT with price = MINT_PRICE (1 ether)
+     * @return Incremented `currentTokenId`
+     */
+    function mint() public payable returns (uint256) {
+        require(msg.value == MINT_PRICE, "Invalid MINT_PRICE");
+
+        uint256 _currentTokenId = currentTokenId;
+        require(_currentTokenId < SUPPLY, "CAN NOT mint more than SUPPLY");
+        unchecked {
+            currentTokenId = _currentTokenId + 1;
+        }
+        _safeMint(_msgSender(), _currentTokenId);
+
+        return _currentTokenId;
+    }
+
+    function withdrawEther() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+}
